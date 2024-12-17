0373e66dcd68871eabe83efca18f63ff506fe434
Supriya Sarjerao Dandawate <141334510+Supriyadandawate@users.noreply.github.com>
2024-12-17 14:24:58 +0530

    Add files via upload

---
+++ Introduction To Numpy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+# we have python list and we can convert it to Numpy array 
+import numpy as np
+list_01 =[26,87,24,56]
+np.array(list_01)
+
